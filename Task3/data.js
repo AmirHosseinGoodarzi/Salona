7b2ec3b86dff7493cdeffcfd71c94dc392f19831
AmirGoodarzi <amirgoodarzi.web@gmail.com>
2024-10-24 18:06:39 +0330

    Complete Task 3 :)

---
+++ Task3/data.js
@@ -0,0 +1,93 @@
+const data = [
+  {
+    title: "سالن زیبایی حانا احمدی",
+    imageCover: "./assets/salonasCovers/1.jpg",
+    isFavorited: false,
+    badge: "خدمات تخصصی",
+    workHours: "از شنبه تا جمعه 10 صبح الی 6 عصر",
+    manager: "حانا احمدی",
+    rate: 3
+  },
+  {
+    title: "سالن زیبایی آتی",
+    imageCover: "./assets/salonasCovers/2.jpg",
+    isFavorited: true,
+    badge: "سالن",
+    workHours: "از شنبه تا جمعه 10 صبح الی 6 عصر",
+    manager: "عاطفه طوقانی",
+    rate: 5
+  },
+  {
+    title: "خدمات پوست نیوانی اسکین",
+    imageCover: "./assets/salonasCovers/3.jpg",
+    isFavorited: false,
+    badge: "خدمات تخصصی",
+    workHours: null,
+    manager: "آنیتا مشهدی",
+    rate: 1
+  },
+  {
+    title: "سالن زیبایی نلی",
+    imageCover: "./assets/salonasCovers/4.jpg",
+    isFavorited: false,
+    badge: "سالن",
+    workHours: "از شنبه تا جمعه 10 صبح الی 6 عصر",
+    manager: "نیلوفر خدادادی",
+    rate: 0
+  },
+  {
+    title: "خدمات مژه ریحانه",
+    imageCover: "./assets/salonasCovers/5.jpg",
+    isFavorited: false,
+    badge: "سالن",
+    workHours: "از شنبه تا جمعه 10 صبح الی 6 عصر",
+    manager: "راضیه جعفری",
+    rate: 0
+  },
+  {
+    title: "سالن کیمیا-اسکین",
+    imageCover: "./assets/salonasCovers/6.jpg",
+    isFavorited: false,
+    badge: "سالن",
+    workHours: null,
+    manager: "کیمیا رزم آبادی",
+    rate: 0
+  },
+  {
+    title: "سالن زیبایی شیوانا",
+    imageCover: "./assets/salonasCovers/7.jpg",
+    isFavorited: false,
+    badge: "سالن",
+    workHours: "از شنبه تا جمعه 10 صبح الی 6 عصر",
+    manager: "اعظم اکراد",
+    rate: 3
+  },
+  {
+    title: "سالن زیبایی خانه عروس",
+    imageCover: "./assets/salonasCovers/8.jpg",
+    isFavorited: true,
+    badge: "خدمات تخصصی",
+    workHours: "از شنبه تا جمعه 10 صبح الی 6 عصر",
+    manager: "نیلوفر رضایی",
+    rate: 0
+  },
+  {
+    title: "سالن زیبایی نلی",
+    imageCover: "./assets/salonasCovers/9.jpg",
+    isFavorited: false,
+    badge: "سالن",
+    workHours: "از شنبه تا جمعه 10 صبح الی 6 عصر",
+    manager: "معصومه راد",
+    rate: 0
+  },
+  {
+    title: "سالن زیبایی خانه عروس",
+    imageCover: "./assets/salonasCovers/10.jpg",
+    isFavorited: true,
+    badge: "سالن",
+    workHours: null,
+    manager: "مریم اکبری",
+    rate: 4
+  }
+]
+
